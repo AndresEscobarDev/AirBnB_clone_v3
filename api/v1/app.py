cffc0c9144eac0cc7b43418a01d09c75fdd58b8f
Andres Felipe Escobar Cardona <felipeescobar15@gmail.com>
2020-09-10 01:22:11 +0000

    Tasks 6, 7

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-from flask import Flask, Blueprint
+"""
+app.py
+"""
+
+from flask import Flask, Blueprint, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -11,8 +15,13 @@ app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def close(error):
+    """ Call method close() from storage """
     storage.close()
 
+@app.errorhandler(404)
+def not_found(error):
+    """ Response for error code 404 """
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
     host = getenv("HBNB_API_HOST", "0.0.0.0 ")
